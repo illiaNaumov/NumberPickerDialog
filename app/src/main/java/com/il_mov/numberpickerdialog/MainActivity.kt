912c8cff9dfff79074b98a101d2d7f10aaabb808
il_mov <ilyuadier@gmail.com>
2018-03-01 15:15:59 +0200

    library with NumberPicker was implemented

--- app/src/main/java/com/il_mov/numberpickerdialog/MainActivity.kt
+++ app/src/main/java/com/il_mov/numberpickerdialog/MainActivity.kt
@@ -1,16 +1,9 @@
 package com.il_mov.numberpickerdialog
 
-import android.content.res.Resources
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.support.design.widget.BottomSheetBehavior
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.View
-import android.widget.EditText
-import android.widget.NumberPicker
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_number_picker.*
 
@@ -20,11 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setDividerColor(numberPicker, Color.parseColor("#b7b7b7"))
         numberPicker.maxValue = 10
 
         val bottomSheetBehavior = BottomSheetBehavior.from(bottom_sheet)
-        bottomSheetBehavior.isHideable
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
 
         showPickerButtonView.setOnClickListener {
@@ -37,28 +28,6 @@ class MainActivity : AppCompatActivity() {
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
         } else {
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
-        }
-    }
-
-    private fun setDividerColor(picker: NumberPicker, color: Int) {
-
-        val pickerFields = NumberPicker::class.java.declaredFields
-        for (pf in pickerFields) {
-            if (pf.name == "mSelectionDivider") {
-                pf.isAccessible = true
-                try {
-                    val colorDrawable = ColorDrawable(color)
-                    pf.set(picker, colorDrawable)
-                } catch (e: IllegalArgumentException) {
-                    e.printStackTrace()
-                } catch (e: Resources.NotFoundException) {
-                    e.printStackTrace()
-                } catch (e: IllegalAccessException) {
-                    e.printStackTrace()
-                }
-
-                break
-            }
         }
     }
 }
